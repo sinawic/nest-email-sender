85f27d4007a350b74a74b16d86662d04a43a4b92
sinawic <sina_041_sm@yahoo.com>
2022-06-28 13:30:41 +0430

    mail service merged to mail module

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -2,14 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Email, Attachment } from './schemas';
-import { MailsService } from './mail.service'
+const nodemailer = require('nodemailer')
+
+interface IMailOptions {
+  to: string,
+  from: string | undefined,
+  subject: string,
+  text: string,
+  attachments?: { path: any; }[]
+}
 @Injectable()
 export class EmailsService {
-  constructor(@InjectModel(Email.name) private emailModel: Model<any>,
+  constructor(
+    @InjectModel(Email.name) private emailModel: Model<any>,
     @InjectModel(Attachment.name) private attachmentModel: Model<any>) {
 
-    const mailsService = new MailsService(emailModel)
-    mailsService.init()
+    this.init()
   }
 
   createEmail = async ({ to, subject, text, supporter, room }) => {
@@ -25,6 +33,96 @@ export class EmailsService {
     return await new this.attachmentModel({
       ...file, email
     }).save()
+  }
+
+
+  // a service which listens to database changes
+  // if detects any email with {sent=false}, it will send an email
+  // this is done for simulating queueing
+  // so that the data doesn't get lost in high scales
+  sendMail = (options: IMailOptions) => {
+    console.log('sending from:', options.from, 'sending to:', options.to)
+
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: options.from,
+        pass: process.env.SOURCE_PASSWORD
+      }
+    })
+
+    var mailOptions: IMailOptions = {
+      from: options.from,
+      to: options.to,
+      subject: options.subject,
+      text: options.text,
+      attachments: []
+    }
+
+    if (options.attachments) {
+      options.attachments.map(a => {
+        mailOptions.attachments?.push({ path: a.path })
+      })
+    }
+
+    // add attachments if any
+    if (options.attachments && options.attachments.length > 0) {
+      mailOptions.attachments = options.attachments.map(attachment => ({ path: attachment.path }))
+    }
+
+    return transporter.sendMail(mailOptions, function (error: any, info) {
+      if (error) {
+        throw new Error(error)
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    })
+  }
+
+  init = async () => {
+    const i = process.env.INTERVAL || 5000
+    console.log(`Mail Service is running every ${+i / 1000} seconds...`)
+
+    setInterval(async () => {
+
+      const email = await this.emailModel.aggregate([
+        //@ts-ignore
+        { $match: { sent: false } },
+        { $sort: { 'date_created': 1 } },
+        { $limit: 1 },
+        {
+          $lookup: {
+            from: 'attachments',
+            localField: '_id',
+            foreignField: 'email',
+            as: 'attachments'
+          }
+        },
+        {
+          $lookup: {
+            from: 'rooms',
+            localField: 'room',
+            foreignField: '_id',
+            as: 'room'
+          }
+        }, { $unwind: '$room' }
+      ])
+
+      if (email.length > 0) {
+        const { to, subject, text, attachments, room } = email[0]
+        try {
+          await this.sendMail({ to, subject, text, attachments, from: room.email })
+          await this.emailModel.updateOne({ _id: email[0]._id }, { sent: true })
+        } catch (err) {
+          console.log(err)
+        }
+      } else
+        console.log('No emails to send')
+
+
+    }, +i)
+
   }
 
 }
